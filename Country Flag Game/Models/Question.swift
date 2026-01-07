f110d0543f2fe4aaaee685b917be0c073dd6b17c
Joseph <jjphillipse4@gmail.com>
2026-01-07 10:39:13 -0600

    Created Custom Button

--- Country Flag Game/Models/Question.swift
+++ Country Flag Game/Models/Question.swift
@@ -1,16 +1,17 @@
 //
-//  File.swift
+//  Answer.swift
 //  Country Flag Game
 //
-//  Created by joseph phillips on 1/6/26.
+//  Created by joseph phillips on 1/7/26.
 //
-
 import Foundation
+
 struct  Answer : Identifiable{
     var id = UUID()
     var text:String
     var isCorrect:Bool
 }
+
 struct Question: Identifiable {
     var id = UUID()
     var correctAnswer: Answer
